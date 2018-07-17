6825e56627c2706e8aa0fb9fd127701d9877e8d9
arthur-dysart <arthur.dysart@gmail.com>
2018-07-17 11:31:38 -0400

    v1.0.0 Prototype with README.md and unit tests

--- insight_testsuite/temp/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Runs Pharmacopedia.Py script with default input
-python ./src/Pharmacopedia.py ./input/itcont.txt ./output/top_cost_drug.txt False
-
-# Leaves terminal window open to display script responses. Remove comment tag to ENABLE this feature
-# read -n 1 -s -r -p $'\n(Press any key to exit)\n'
